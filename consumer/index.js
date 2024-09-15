d8e294344ca6d72b5beadcdc766bae03b0324d85
Desmond Obisi <desmondcutest@yahoo.com>
2024-09-15 20:03:21 +0100

    chore: setup poc

---
+++ consumer/index.js
@@ -0,0 +1,114 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { Kafka } from 'kafkajs';
+import twilio from 'twilio';
+import mongoose from 'mongoose';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Kafka consumer configuration
+const kafka = new Kafka({
+ clientId: 'app-consumer',
+ brokers: ['kafka:29092'],
+});
+
+const consumer = kafka.consumer({
+ groupId: 'transactions-group',
+ retry: {
+  retries: 8, // Increase the number of retries
+ },
+ requestTimeout: 60000, // Increase the request timeout
+});
+
+// Twilio configuration
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_AUTH_TOKEN;
+const twilioClient = new twilio(accountSid, authToken);
+
+// MongoDB configuration
+const mongoUrl = process.env.MONGO_URL;
+const dbName = process.env.DB_NAME;
+let db;
+
+// Mongoose connection to MongoDB
+mongoose
+ .connect(`${mongoUrl}/${dbName}`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+ })
+ .then(() => console.log('Connected to MongoDB'))
+ .catch((err) => console.error('MongoDB connection error:', err));
+
+// Add health check route
+app.get('/health', (req, res) => {
+ res.status(200).json({ status: 'OK', message: 'Consumer service is healthy' });
+});
+
+// Function to send SMS alert via Twilio
+const sendAlert = async (transaction) => {
+ const message = `Alert: A transaction of $${transaction.amount} has been made at ${transaction.location}.`;
+ await twilioClient.messages
+  .create({
+   body: message,
+   from: 'your_twilio_phone_number',
+   to: 'recipient_phone_number',
+  })
+  .then((message) => console.log('Alert sent:', message.sid))
+  .catch((err) => console.error('Error sending alert:', err));
+};
+
+// Save transaction to MongoDB
+const saveTransactionToMongo = async (transaction) => {
+ const collection = await db.collection('transactions');
+ collection
+  .insertOne(transaction)
+  .then((result) =>
+   console.log('Transaction saved to MongoDB:', result.insertedId)
+  )
+  .catch((err) => console.error('Error saving transaction to MongoDB:', err));
+};
+
+const processMessage = async (message) => {
+ const transaction = JSON.parse(message.value);
+ console.log('Transaction received:', transaction);
+
+ // Save transaction to MongoDB
+ await saveTransactionToMongo(transaction);
+
+ // Send an alert for high-value transactions
+ if (transaction.amount > 5000000) {
+  await sendAlert(transaction);
+ }
+};
+
+// Kafka consumer event for incoming messages
+const startConsumer = async () => {
+ await consumer.connect();
+ await consumer.subscribe({ topic: 'transaction-data', fromBeginning: true });
+
+ await consumer.run({
+  eachMessage: async ({ message }) => {
+   await processMessage(message);
+  },
+ });
+};
+
+// Handle consumer errors
+consumer.on('error', (err) => {
+ console.error('Consumer error:', err);
+});
+
+process.on('SIGINT', async () => {
+ console.log('Shutting down gracefully...');
+ await consumer.disconnect();
+ process.exit(0);
+});
+
+startConsumer().catch(console.error);
+
+app.listen(process.env.PORTB, () => {
+ console.log('Weather Consumer running on port', process.env.PORTB);
+});
---
+++ producer/index.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import { Kafka } from 'kafkajs';
+import { faker } from '@faker-js/faker';
+import cron from 'node-cron';
+
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+
+// Kafka producer configuration
+const kafka = new Kafka({
+ clientId: 'app-producer',
+ brokers: ['kafka:29092'],
+});
+
+const producer = kafka.producer();
+
+// Add health check route
+app.get('/health', (req, res) => {
+ res.status(200).json({ status: 'OK', message: 'Producer service is healthy' });
+});
+
+// Kafka producer event for sending data
+async function sendToKafka(data) {
+ try {
+  await producer.connect();
+  const messages = data.map((item) => ({
+   value: JSON.stringify(item, (key, value) =>
+    typeof value === 'bigint' ? value.toString() : value
+   ),
+  }));
+  await producer.send({
+   topic: 'weather-data',
+   messages,
+  });
+ } catch (error) {
+  console.error('Error sending data to Kafka:', error);
+ } finally {
+  await producer.disconnect();
+ }
+}
+
+// Generate and send transaction data to Kafka every 15 minutes
+const generateTransactions = async () => {
+ let transactions = [];
+ for (let i = 0; i < 20; i++) {
+  let transaction = {
+   id: faker.string.uuid(),
+   amount: faker.finance.amount(100, 10000, 2),
+   accountName: faker.finance.accountName(),
+   accountNumber: faker.finance.accountNumber(5),
+   timestamp: new Date().toISOString(),
+   location: faker.location.city(),
+  };
+  transactions.push(transaction);
+ }
+
+ if (transactions.length > 0) {
+  await sendToKafka(transactions);
+ }
+ return transactions;
+};
+
+// Initial run
+await generateTransactions().then((data) => {
+ if (data) {
+  console.log('Initial weather data sent to Kafka:', data);
+ }
+});
+
+// Schedule the task to run every 15 minutes
+cron.schedule('*/15 * * * *', async () => {
+ const transactionData = await generateTransactions();
+ if (transactionData) {
+  console.log('Weather data sent to Kafka:', transactionData);
+ }
+});
+
+app.listen(process.env.PORTA, () => {
+ console.log('Weather Producer running on port', process.env.PORTA);
+});
